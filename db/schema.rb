f2a3acc3d4d1c94572e45a3f81dda8a499a39e25
richard lumpi <richard.lumpi@sbg.at>
2016-08-03 12:00:41 +0100

    created reviews model and changed phone_number column in restaurants to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803104208) do
+ActiveRecord::Schema.define(version: 20160803105808) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.integer  "phone_number"
+    t.string   "phone_number"
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "rating"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
 
 end
